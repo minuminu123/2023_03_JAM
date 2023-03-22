c30f4917020e5417de09e7a0c7ceeed36bd00cf1
minuminu123 <alsdn96321@gmail.com>
2023-03-22 18:00:12 +0900

    회원가입 구현중, 비밀번호 체크, 필수 입력

--- src/com/KoreaIT/example/JAM/App.java
+++ src/com/KoreaIT/example/JAM/App.java
@@ -63,7 +63,82 @@ public class App {
 			return -1;
 		}
 
-		if (cmd.equals("article write")) {
+		if (cmd.equals("member join")) {
+			String loginId = null;
+			String loginPw = null;
+			String loginPwConfirm = null;
+			String name = null;
+
+			System.out.println("==회원 가입==");
+
+			while (true) {
+				System.out.printf("아이디 : ");
+				loginId = sc.nextLine().trim();
+
+				if (loginId.length() == 0) {
+					System.out.println("아이디를 입력해주세요");
+					continue;
+				}
+				break;
+			}
+
+			while (true) {
+				System.out.printf("비밀번호 : ");
+				loginPw = sc.nextLine().trim();
+
+				if (loginPw.length() == 0) {
+					System.out.println("비밀번호를 입력해주세요");
+					continue;
+				}
+
+				boolean loginPwCheck = true;
+
+				while (true) {
+					System.out.printf("비밀번호 확인 : ");
+					loginPwConfirm = sc.nextLine().trim();
+
+					if (loginPwConfirm.length() == 0) {
+						System.out.println("비밀번호 확인을 입력해주세요");
+						continue;
+					}
+
+					if (loginPw.equals(loginPwConfirm) == false) {
+						System.out.println("비밀번호가 일치하지 않습니다. 다시 입력해주세요");
+						loginPwCheck = false;
+						break;
+					}
+					break;
+				}
+				if (loginPwCheck) {
+					break;
+				}
+			}
+
+			while (true) {
+				System.out.printf("이름 : ");
+				name = sc.nextLine();
+
+				if (name.length() == 0) {
+					System.out.println("이름을 입력해주세요");
+					continue;
+				}
+				break;
+			}
+
+			SecSql sql = new SecSql();
+
+			sql.append("INSERT INTO `member`");
+			sql.append("SET regDate = NOW()");
+			sql.append(", updateDate = NOW()");
+			sql.append(", loginId = ?", loginId);
+			sql.append(", loginPw = ?", loginPw);
+			sql.append(", `name` = ?", name);
+
+			int id = DBUtil.insert(conn, sql);
+
+			System.out.println(id + "번 회원님, 가입 되었습니다");
+
+		} else if (cmd.equals("article write")) {
 			System.out.println("==게시물 작성==");
 			System.out.printf("제목 : ");
 			String title = sc.nextLine();
@@ -135,24 +210,13 @@ public class App {
 		} else if (cmd.startsWith("article modify ")) {
 			int id = Integer.parseInt(cmd.split(" ")[2]);
 
-			SecSql sql = new SecSql();
-			sql.append("SELECT COUNT(*)");
-			sql.append("FROM article");
-			sql.append("WHERE id = ?", id);
-
-			int articlesCount = DBUtil.selectRowIntValue(conn, sql);
-
-			if (articlesCount == 0) {
-				System.out.println(id + "번 글은 존재하지 않습니다");
-				return 0;
-			}
 			System.out.println("==게시물 수정==");
 			System.out.printf("새 제목 : ");
 			String title = sc.nextLine();
 			System.out.printf("새 내용 : ");
 			String body = sc.nextLine();
 
-			sql = new SecSql();
+			SecSql sql = new SecSql();
 
 			sql.append("UPDATE article");
 			sql.append("SET updateDate = NOW()");
